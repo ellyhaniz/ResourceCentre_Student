d088d5af1b41b29aadd8cb6081cfb268253cd0b6
19022315 <19022315@DESKTOP-6I1IGS6>
2020-07-21 15:42:28 +0800

    test3

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -250,7 +250,7 @@ public class ResourceCentre {
 		
 		
 	}
-	//sgsr
+
 	//================================= Option 4 Return =================================
 	public static boolean doReturnCamcorder(ArrayList<Camcorder> camcorderList,String tag) {
 		boolean isReturned = false;
