9b98422d82460836eebf89cd9c21fc6f3aee9514
19009491 <19009491@DESKTOP-P36R3F4>
2020-07-21 15:39:42 +0800

    Save

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -251,11 +251,22 @@ public class ResourceCentre {
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
 		boolean isReturned = false;
 		// write your code here
+		
+		for (int i = 0; i < chromebookList.size(); i++) {
+			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag()));
+	        chromebookList.get(i).setIsAvailable(true);
+	        chromebookList.get(i).setDueDate("");
+	        isReturned=true;
+			
+		}
 		return isReturned;
 	}
 	public static void returnChromebook(ArrayList<Chromebook> chromebookList) {
 		// write your code here
 		// write your code here
+		ResourceCentre.viewAllChromebook(chromebookList);
+		String tag = Helper.readString("enter asset tag > ");
+		Boolean isReturned = doReturnChromebook(chromebookList,tag);
 	}
 
 
