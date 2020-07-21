0a24f6dabd84cc6f4fb8e56e7e2c6d369f2c852f
19022315 <19022315@DESKTOP-6I1IGS6>
2020-07-21 14:49:07 +0800

    update doLoan() in ResourceCentre

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -145,8 +145,7 @@ public class ResourceCentre {
 
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
-
-
+		
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
