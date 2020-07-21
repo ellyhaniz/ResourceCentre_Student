b9d132ac026710eccd81c00767d89c8c9015d03f
19043192 <19043192@DESKTOP-S09FELA.lan>
2020-07-21 14:21:49 +0800

    update retrieveAllChromebook() in ResourceCentre

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -150,6 +150,7 @@ public class ResourceCentre {
 
 			output += String.format("%-10s %-30s %-10s %-10s %-20d\n", chromebookList.get(i).getAssetTag(),
 					chromebookList.get(i).getDescription(), 
+					
 					ResourceCentre.showAvailability(chromebookList.get(i).getIsAvailable()),
 					
 					chromebookList.get(i).getDueDate(),chromebookList.get(i).getOs());
